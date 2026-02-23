c918d22493e939b1af83c9b20f0025454e4643b6
Paula Ngcobo <lindokuhlepaula@email.com>
2026-02-23 22:18:36 +0200

    Revert router to BrowserRouter as requested

--- src/RootApp.tsx
+++ src/RootApp.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
